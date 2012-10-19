cb41e22ea0e3102dc33bb4ffc541c3b18bdb0a7f
Magdalena Biala <magdalena.biala@sentaca.com>
2012-10-19 18:51:57 +0200

    ATTSA-11

--- att-paas-app/src/com/platform/c2125126633/alerts/AlertsDataPolicy.java
+++ att-paas-app/src/com/platform/c2125126633/alerts/AlertsDataPolicy.java
@@ -1,7 +1,11 @@
 package com.platform.c2125126633.alerts;
 
-import com.platform.api.Parameters;
+import com.platform.api.Functions;
 import com.platform.api.Logger;
+import com.platform.api.Parameters;
+import com.platform.api.Result;
+import com.platform.c2125126633.gsms.GSMSManager;
+import com.platform.c2125126633.locaid.LocAidManager;
 
 /**
  * Class that implements data policies used by Alerts object.
@@ -10,8 +14,28 @@ import com.platform.api.Logger;
  * 
  */
 public class AlertsDataPolicy {
-	public void processAlert(Parameters requestParams) {
-		Logger.info("New failure alert received!", this.getClass());
-	}
+  public void processAlert(Parameters requestParams) {
+    Logger.info("New failure alert received!", this.getClass());
+    try {
+      LocAidManager locAidManager = new LocAidManager();
+      String lat = (String) requestParams.getObject("latitude");
+      String lng = (String) requestParams.getObject("longitude");
+      Logger.debug("requestParams: " + requestParams, AlertsDataPolicy.class);
+      String msisdn = locAidManager.locateEngineer(lat, lng);
+      Logger.debug("Chosen msisdn: " + msisdn, AlertsDataPolicy.class);
+      Result result = Functions.searchRecords("Engineers", "id", "msisdn=" + msisdn);
+      Logger.debug("Result: " + result.getIterator().next().toString(), AlertsDataPolicy.class);
+      String engineerId = result.getIterator().next().get("id");
+      Parameters params = new Parameters();
+      params.add("related_to_engineers", engineerId);
+      params.add("status", "ASSIGNED");
+      String alertId = (String) requestParams.getObject("id");
+      Functions.updateRecord("Alerts", alertId, params);
+      GSMSManager gsmsManager = new GSMSManager();
+      gsmsManager.sendSms(msisdn, (String) requestParams.getObject("device_id"), Double.valueOf(lat), Double.valueOf(lng));
+    } catch (Exception e) {
+      Logger.error(e.getMessage(), AlertsDataPolicy.class);
+    }
+  }
 
 }
--- att-paas-app/src/com/platform/c2125126633/locaid/LocAidManager.java
+++ att-paas-app/src/com/platform/c2125126633/locaid/LocAidManager.java
@@ -7,8 +7,10 @@ import com.att.enablers.locaid.LatitudeLongitudeServices;
 import com.att.enablers.locaid.RegistrationServices;
 import com.att.enablers.locaid.request.ClassIDList;
 import com.att.enablers.locaid.response.BaseResponseBean;
-import com.att.enablers.locaid.response.BaseTransactionResponseBean;
+import com.att.enablers.locaid.response.ComplexClassIDResponseBean;
+import com.att.enablers.locaid.response.LocationAnswerResponseBean;
 import com.att.enablers.locaid.response.LocationResponseBean;
+import com.att.enablers.locaid.response.MsisdnStatusResponseBean;
 import com.att.enablers.locaid.response.PhoneStatusListResponseBean;
 import com.att.enablers.locaid.response.SubscribePhoneAllResponseBean;
 import com.att.enablers.locaid.response.SubscribePhoneResponseBean;
@@ -104,6 +106,12 @@ public class LocAidManager {
    * @return
    * @throws Exception
    */
+  /**
+   * @param command
+   * @param selectedPhones
+   * @return
+   * @throws Exception
+   */
   protected SubscribePhoneResponseBean register(String command, List<String> selectedPhones) throws Exception {
     LocAidSetup setup = getLocAidSetup();
 
@@ -118,6 +126,45 @@ public class LocAidManager {
 
     RegistrationServices service = new RegistrationServices(setup.getRegistrationServiceUrl(), setup.getLogin(), setup.getPassword());
     SubscribePhoneResponseBean subscribeResponse = service.subscribePhone(command, classIdListObj);
+
+    // Update subscription status
+    if (subscribeResponse.getError() == null) {
+      List<ComplexClassIDResponseBean> classIdListResponse = subscribeResponse.getClassIdList();
+      if (classIdListResponse != null) {
+        for (ComplexClassIDResponseBean classIdResponse : classIdListResponse) {
+          if (classIdResponse.getError() == null) {
+            List<MsisdnStatusResponseBean> msisdnStatusList = classIdResponse.getMsisdnList();
+            if (msisdnStatusList != null) {
+              for (MsisdnStatusResponseBean msisdnStatus : msisdnStatusList) {
+                String msisdnReturned = msisdnStatus.getMsisdn();
+                if (msisdnStatus.getError() == null) {
+                  Result result = Functions.searchRecords("Engineers", "id", "msisdn=" + msisdnReturned);
+                  int code = result.getCode();
+                  if (code > 0) {
+                    String id = result.getIterator().next().get("id");
+                    Logger.info("id: " + id, LocAidManager.class);
+                    String subscriptionStatus = null;
+                    if (msisdnStatus.getStatus().equals("OK")) {
+                      if (command.equals("OPTIN")) {
+                        subscriptionStatus = "OPTIN_PENDING";
+                      } else if (command.equals("YES")) {
+                        subscriptionStatus = "OPTIN_COMPLETE";
+                      } else if (command.equals("CANCEL")) {
+                        subscriptionStatus = "CANCELLED";
+                      }
+                    }
+                    Logger.info("subscriptionStatus: " + subscriptionStatus, LocAidManager.class);
+                    Parameters params = new Parameters();
+                    params.add("lis_subscription_status_2125126633", subscriptionStatus);
+                    Functions.updateRecord("Engineers", id, params);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
 
     return subscribeResponse;
   }
@@ -177,12 +224,12 @@ public class LocAidManager {
    * @return
    * @throws Exception
    */
-  protected BaseTransactionResponseBean latlongMultiple(List<String> selectedPhones, String coorType, String locationMethod, String syncType, String overage)
+  protected LocationAnswerResponseBean latlongMultiple(List<String> selectedPhones, String coorType, String locationMethod, String syncType, String overage)
       throws Exception {
     LocAidSetup setup = getLocAidSetup();
 
     LatitudeLongitudeServices service = new LatitudeLongitudeServices(setup.getLocationServiceUrl(), setup.getLogin(), setup.getPassword(), setup.getClassId());
-    BaseTransactionResponseBean locationResponseObj = service.getLocationsX(selectedPhones, coorType, locationMethod, syncType, Integer.valueOf(overage));
+    LocationAnswerResponseBean locationResponseObj = service.getLocationsX(selectedPhones, coorType, locationMethod, syncType, Integer.valueOf(overage));
 
     return locationResponseObj;
   }
@@ -199,6 +246,71 @@ public class LocAidManager {
     BaseResponseBean locationResponseObj = service.getLocationsAnswer(Long.parseLong(transactionId));
 
     return locationResponseObj;
+  }
+
+  public String locateEngineer(String lat, String lng) throws Exception {
+    String msisdn = null;
+    Result result = Functions.searchRecords("Engineers", "id, msisdn", "lis_subscription_status_2125126633='OPTIN_COMPLETE'");
+    int resultCode = result.getCode();
+    Logger.info("Result Code: " + resultCode, LocAidManager.class);
+    if (resultCode < 0) {
+      // Some error happened.
+      String msg = "Engineers could not be retrieved";
+      Functions.debug(msg + ":\n" + result.getMessage()); // Log details
+      Functions.throwError(msg + "."); // Error dialog
+    } else if (resultCode == 0) {
+      // No records found. Take action according to your business logic
+    } else {
+      // Records retrieved successfully
+      List<String> msisdnList = new ArrayList<String>();
+      ParametersIterator iterator = result.getIterator();
+      while (iterator.hasNext()) {
+        Parameters params = iterator.next();
+        msisdnList.add(params.get("msisdn"));
+      }
+      Logger.debug(msisdnList, LocAidManager.class);
+      LocationAnswerResponseBean response = latlongMultiple(msisdnList, "DECIMAL", "LEAST_EXPENSIVE", "syn", "1");
+      List<LocationResponseBean> list = response.getLocationResponse();
+      double distance = Double.MAX_VALUE;
+      Logger.debug("list.size(): " + list.size(), LocAidManager.class);
+      for (LocationResponseBean bean : list) {
+        if (bean.getStatus().equals("FOUND")) {
+          Logger.debug("Status:" + bean.getStatus(), LocAidManager.class);
+          double actualDistance = countDistance(Double.valueOf(lat), Double.valueOf(lng), Double.valueOf(bean.getCoordinateGeo().getY()),
+              Double.valueOf(bean.getCoordinateGeo().getX()));
+          Logger.debug("Msisdn: " + bean.getNumber(), LocAidManager.class);
+          Logger.debug("Coordinates: " + bean.getCoordinateGeo().getY() + ", " + bean.getCoordinateGeo().getX(), LocAidManager.class);
+          Logger.debug("Distance: " + actualDistance, LocAidManager.class);
+          if (actualDistance < distance) {
+            distance = actualDistance;
+            msisdn = bean.getNumber();
+          }
+        }
+      }
+    }
+    return msisdn;
+  }
+
+  public static double countDistance(double lat1, double long1, double lat2, double long2) {
+    // var R = 6371; // km
+    double R = 6371;
+    // var dLat = (lat2 - lat1).toRad();
+    double dLat = Math.toRadians(lat2 - lat1);
+    // var dLon = (lon2 - lon1).toRad();
+    double dLon = Math.toRadians(long2 - long1);
+
+    // var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.sin(dLon / 2) *
+    double a = Math.sin(dLat / 2.0) * Math.sin(dLat / 2.0) + Math.sin(dLon / 2.0) *
+    // Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        Math.sin(dLon / 2.0) * Math.cos(lat1) * Math.cos(lat2);
+
+    // var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    double c = 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    // var d = R * c;
+    double d = R * c;
+    // return d;
+
+    return d;
   }
 
 }
